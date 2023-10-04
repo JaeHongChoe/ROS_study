3b60a812141363ada12aa3a116e9fbacf8c7f65e
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2023-10-04 20:58:44 +0900

    Add files via upload

---
+++ devel/share/common-lisp/ros/msg_send/msg/_package_my_msg.lisp
@@ -0,0 +1,14 @@
+(cl:in-package msg_send-msg)
+(cl:export '(FIRST_NAME-VAL
+          FIRST_NAME
+          LAST_NAME-VAL
+          LAST_NAME
+          AGE-VAL
+          AGE
+          SCORE-VAL
+          SCORE
+          PHONE_NUMBER-VAL
+          PHONE_NUMBER
+          ID_NUMBER-VAL
+          ID_NUMBER
+))
