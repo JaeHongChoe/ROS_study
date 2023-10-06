fc87230d2bec9c7a00d81b1da7d3ed6163ad8f2d
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2023-10-06 15:23:15 +0900

    Add files via upload

---
+++ src/rviz_xycar/src/rviz_8_drive.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import rospy
+import time
+from xycar_msgs.msg import xycar_motor
+
+motor_c = xycar_motor()
+
+rospy.init_node('auto_drive')
+
+pub = rospy.Publisher('xycar_motor', xycar_motor, queue_size = 1)
+
+def motor_pub(angle,speed):
+   global pub
+   global motor_c
+
+   motor_c.angle = angle
+   motor_c.speed = speed
+
+   pub.publish(motor_c)
+
+speed = 3
+
+while not rospy.is_shutdown():
+   angle = -50
+   for i in range(40):
+      motor_pub(angle, speed)
+      time.sleep(0.1)
+
+   angle = 0
+   for i in range(30):
+      motor_pub(angle, speed)
+      time.sleep(0.1)
+
+   angle = 50
+   for i in range(40):
+      motor_pub(angle, speed)
+      time.sleep(0.1)
+
+   angle = 0 
+   for i in range(30):
+      motor_pub(angle, speed)
+      time.sleep(0.1)
